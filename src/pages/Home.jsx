9fb37b3f31d365ab04822ddf38808f0d7a815343
Berzki <mhdrizky32@gmail.com>
2026-08-14 22:07:13 +0700

    feat(home): compose home and not-found pages on the new layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Restaurant from '../components/Restaurant'
-import Footer from '../components/Footer'
-import Brand from '../components/Brand'
-import About from '../components/About'
-import Book from '../components/Book'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import PageLayout from "../components/templates/PageLayout";
+import Hero from "../components/organisms/Hero";
+import BrandMarquee from "../components/organisms/BrandMarquee";
+import AboutSection from "../components/organisms/AboutSection";
+import RestaurantListing from "../components/organisms/RestaurantListing";
+import ReservationBanner from "../components/organisms/ReservationBanner";
+
+const SCROLL_DELAY = 80;
+
+const scrollToSection = (id) =>
+  document.getElementById(id)?.scrollIntoView({ behavior: "smooth", block: "start" });
 
 function Home() {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <Brand />
-            <About />
-            <Restaurant />
-            <Book />
-            <Footer />
-        </>
-    )
+  const [query, setQuery] = useState("");
+  const { state, hash } = useLocation();
+
+  useEffect(() => {
+    const target = state?.scrollTo || hash.replace("#", "");
+    if (!target) return undefined;
+
+    const timer = setTimeout(() => scrollToSection(target), SCROLL_DELAY);
+    return () => clearTimeout(timer);
+  }, [state, hash]);
+
+  return (
+    <PageLayout overlayNav>
+      <Hero
+        query={query}
+        onQueryChange={setQuery}
+        onSearch={() => scrollToSection("restaurants")}
+      />
+      <BrandMarquee />
+      <AboutSection />
+      <RestaurantListing query={query} onQueryChange={setQuery} />
+      <ReservationBanner />
+    </PageLayout>
+  );
 }
 
-export default Home
+export default Home;
